f11e41c939496e7ee371cfad040b22c4f4759d2a
vie_bcit <vienna_ly@bcit.ca>
2025-09-05 15:08:34 -0700

    clean up delate unused

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './default.css';
-import 'jquery'; // This triggers expose-loader
-
-require('./libs/jquery-ui.min.js');
-require('./libs/jquery.ui.touch-punch.min.js');
-require('./libs/three.min.js');
-require('./PanoWithClickObjs.js');
